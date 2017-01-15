3c700657d661e6781e5a54402479cb7264700268
Raahim <ali.raahim@gmail.com>
2017-01-15 12:16:47 +0000

    dbchanges

--- connect.php
+++ connect.php
@@ -1,8 +1,22 @@
 <?php
 
 require_once 'helpers/meekrodb.2.3.class.php';
-DB::$user = 'marspace';
-DB::$password = '';
-DB::$dbName = 'LocalLeadGen';
+
+$prod = false;
+
+if (!$prod)
+{
+    DB::$host = 'localhost';
+    DB::$user = 'marspace';
+    DB::$password = '';
+    DB::$dbName = 'LocalLeadGen';  
+} else {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    DB::$host = $url["host"];
+    DB::$user = $url["user"];
+    DB::$password = $url["pass"];
+    DB::$dbName = substr($url["path"], 1);
+}
+
 
 ?>
