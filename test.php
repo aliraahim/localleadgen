7437404e8d173388041eb643e0de5c51b53342e3
aliraahim <ali.raahim@gmail.com>
2018-03-24 23:35:58 +0500

    Modified test.php

--- test.php
+++ test.php
@@ -3,19 +3,3 @@
 //include('scrape_emails.php');
 
 //GetEmails("business.referrizer.com", true, true);
-
-$url = "mysql://u83tjpeh66scfmyb:twtz13gf0f92f1xc@gp96xszpzlqupw4k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/lffnb70px4i8yl68";
-$dbparts = parse_url($url);
-
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
-
-echo $hostname;
-echo "\n";
-echo $username;
-echo "\n";
-echo $password;
-echo "\n";
-echo $database;
